3c94a0437d297f24d6bce75827975f3137cf6afe
kit <dhinkit@gmail.com>
2023-08-14 14:45:24 -0600

    changed twitter link to youtube in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,8 @@ export default function Footer(){
         </a>
     </div>
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center justify-center">
-        <a className="hover:animate-pulse" href="https://github.com/kitdhing" target="_blank" rel="noreferrer">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 ">Twitter</span>
+        <a className="hover:animate-pulse" href="https://www.youtube.com/channel/UCBjbcwB65z_J54yCCA3Yjyg" target="_blank" rel="noreferrer">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 ">Youtube</span>
         </a>
     </div>
 </footer>
