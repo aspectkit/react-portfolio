5744d716e93a39432430ab6d60059eba8b6b82c5
kit <dhinkit@gmail.com>
2023-10-13 22:49:11 -0600

    changed a tag for sports ball

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -65,7 +65,7 @@ export default function Projects(){
         </div>
         <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Unity</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Unity Remote 5</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#iOS Development</span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Csharp</span>
         </div>
     </div>
